203bc50b0699f763c6282d7678b0f67c97a49594
Mateus Luiz <mateuslfreitasb@gmail.com>
2026-08-05 08:40:39 -0300

    Change sprite view angle

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <SDL3/SDL_surface.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3_image/SDL_image.h>
+#include <cmath>
+#include <vector>
 #include <stdio.h>
 #include <iostream>
 
@@ -16,12 +18,24 @@ void print(const T& arg) {
     std::cout << arg << std::endl;
 }
 
+struct Sprite {
+    float pos_x;
+    float pos_y;
+    float w;
+    float h;
+    float y_offset;
+    float pitch_angle;
+};
+
 int main(int arhc, char* argv[]) {
     print("Starting");
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("Sprite Stacking test", 1200, 800, SDL_WINDOW_OPENGL);
+    int window_width = 1200;
+    int window_height = 800;
+
+    SDL_Window* window = SDL_CreateWindow("Sprite Stacking test", window_width, window_height, SDL_WINDOW_OPENGL);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
 
     SDL_Event event;
@@ -30,52 +44,86 @@ int main(int arhc, char* argv[]) {
     // Sprite Rendering
     SDL_Texture* texture = IMG_LoadTexture(renderer, "./spritesheet.png");
 
-    // float width;
-    // float height;
-    // SDL_GetTextureSize(texture, &width, &height);
-    //
-    // std::cout << width << std::endl;
-    // std::cout << height << std::endl;
+    int sprite_count = 2;
+    float sprite_width = 146;
+    float sprite_height = 146;
+    float current_layer_gap = 0;
+    float pitch_angle = 0.785f;
+    float layer_gap = 10.0;
 
-    float posX = 0;
-    float posY = 0;
-    float w = 146;
-    float h = 146;
+    // Create sprites 
+    std::vector<Sprite> sprites = {};
 
-    SDL_FRect scRect = {
-        posX,
-        posY,
-        w,
-        h
-    };
+    for (int i = 0; i < sprite_count; i++) {
+        Sprite sprite = {
+            0 + (float) (i * sprite_width),
+            0,
+            sprite_width,
+            sprite_height,
+            current_layer_gap,
+            pitch_angle
+        };
+        sprites.push_back(sprite); 
 
-    SDL_FRect dsRect = {
-        posX,
-        posY,
-        w,
-        h
-    };
+        current_layer_gap += layer_gap;
+    }
 
     // Main Loop
     while(true) {
+        // Update
         while(SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 exit = true;
             }
+
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+                if (event.key.key == SDLK_UP) {
+                    for (int i = 0; i < sprite_count; i++) {
+                        sprites[i].pitch_angle += 0.1;
+                    }
+                }
+
+                if (event.key.key == SDLK_DOWN) {
+                    for (int i = 0; i < sprite_count; i++) {
+                        sprites[i].pitch_angle -= 0.1;
+                    }
+                }
+            }
         }
 
-        // Update
-
         // Render
-        SDL_RenderTextureRotated(
-            renderer,
-            texture,
-            &scRect,
-            &dsRect,
-            0.0,
-            nullptr,
-            SDL_FLIP_NONE
-        );
+        SDL_RenderClear(renderer);
+ 
+        for (int i = 0; i < sprite_count; i++) {
+            Sprite sprite = sprites[i];
+
+            SDL_FRect scRect = {
+                sprite.pos_x,
+                sprite.pos_y,
+                sprite.w,
+                sprite.h
+            };
+
+            float dsPosX = (window_width / 2.0) - (sprite.w / 2);
+            float dsPosY = (window_height / 2.0) - (sprite.h / 2);
+
+            SDL_FRect dsRect = {
+                dsPosX,
+                dsPosY - sprite.y_offset,
+                sprite.w,
+                sprite.h * sinf(sprite.pitch_angle)
+            };
+
+            SDL_RenderTextureRotated(
+                renderer,
+                texture,
+                &scRect,
+                &dsRect,
+                0.0,
+                nullptr,
+                SDL_FLIP_NONE
+            );
+        }
 
         if (exit) {
             break;
